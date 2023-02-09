1dba347b11edf76c850e1536797795a585fa34d2
Miguel Chachati <miguelchra@unisabana.edu.co>
2023-02-09 11:27:29 -0500

    Patron de diseño Estructural

---
+++ src/adapter_method/Fighter.java
@@ -0,0 +1,14 @@
+package adapter_method;
+
+public class Fighter {
+    private int phisycalAttack;
+    public Fighter(int phisycalAttack) {
+        this.phisycalAttack = phisycalAttack;
+    }
+    public Fighter(){
+    }
+
+    public int getPhisycalAttack() {
+        return phisycalAttack;
+    }
+}
---
+++ src/adapter_method/Wizard.java
@@ -0,0 +1,12 @@
+package adapter_method;
+
+public class Wizard {
+
+    private double magicAttack;
+    public Wizard (int magicAttack) {
+        this.magicAttack = magicAttack;
+    }
+    public double getMagicalAttack() {
+        return magicAttack;
+    }
+}
